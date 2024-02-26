dac2f156181a72ca78c38458bc6b9f8d35ec38d0
6-Isa-9 <hi.isa.sheriff@gmail.com>
2024-02-26 22:23:32 +0400

    Added firebase dbms, found logic for user register and login

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 
+# Database
+import firebase_admin
+from firebase_admin import credentials, db, auth
+
+# Initializing database
+cred = credentials.Certificate("codebase-secret.json")
+firebase_admin.initialize_app(cred, {"databaseURL": "https://codebase-93435-default-rtdb.europe-west1.firebasedatabase.app/"})
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,14 +16,42 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    # LOGIC FOR LOGIN
+
+    #user = auth.get_user_by_email("user@example.com")
+    #print(user.uid)
+
     return render_template("login.html")
 
 @app.route('/register')
 def register():
+
+    # LOGIC FOR CREATING USER
+
+    #new_user = auth.create_user(
+    #    email = "user@example.com",
+    #    password = "ligma1024",
+    #)
+
+    #auth.update_user(
+    #    new_user.uid,
+    #    display_name = "Ligma Man",
+    #    phone_number = "+971549647315",
+    #    email_verified = False,
+    #    photo_url = "temp photo"
+    #)
+
     return render_template("register.html")
 
 @app.route('/discover')
 def discover():
+
+    # LOGIC FOR DISCOVER
+
+    #ref = db.reference("/users")
+    #ref.update({"Chupapi": {"age": "6", "ball size": "2mm", "ball height": "0.1mm"}})
+
     return render_template("discover.html")
 
 @app.route('/home')
@@ -24,3 +60,7 @@ def homecomp():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
