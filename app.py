88dafadf8dc460c6a91f635c93bd3cbf0f891dab
6-Isa-9 <hi.isa.sheriff@gmail.com>
2024-02-27 18:24:49 +0400

    Fixed recaptcha issue and finished User registration form with basic error validation

--- app.py
+++ app.py
@@ -10,19 +10,25 @@ from firebase_admin import credentials, db, auth
 cred = credentials.Certificate("codebase-secret.json")
 firebase_admin.initialize_app(cred, {"databaseURL": "https://codebase-93435-default-rtdb.europe-west1.firebasedatabase.app/"})
 
+# Initializing flask app
 app = Flask(__name__)
 
+# Applying flask configurations     --- To be kept secret ---
 app.config['SECRET_KEY'] = 'h#@hbJHB$@uygAHB!3137yugas_niGaJKH@#nlNAUBKJ~/AS,.69<>ASDfl..911,aSFOJ'
 app.config['RECAPTCHA_ENABLED'] = True
-app.config['RECAPTCHA_SITE_KEY'] = '6LcxaoEpAAAAAH92Ayj9QRJnLO8FRHDulED4OZOY'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcxaoEpAAAAAH92Ayj9QRJnLO8FRHDulED4OZOY'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcxaoEpAAAAAEOeRtpgYt_PDprYUmkVw_ryUl2p'
 
+# Initialize recaptcha
 recaptcha = ReCaptcha(app)
 
+
+# HOME
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# LOGIN
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
@@ -33,34 +39,35 @@ def login():
 
     return render_template("login.html")
 
+# USER REGISTER
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = UserRegistrationForm()
 
-    if form.validate_on_submit():
-        # If captcha failed
-        #if not recaptcha.verify():
-        #    print("ReCaptcha failed.")
-        # If captcha success
-        #else:
-            # If password != confirm password
-        if form.password != form.confirm_password:
+    if form.validate_on_submit() and recaptcha.verify():
+        # If password != confirm password
+        if form.password.data != form.confirm_password.data:
             print("Passwords do no match.")
-        
-        new_user = auth.create_user(
-            email = form.email.data,
-            password = form.password.data
-        )
-
-        auth.update_user(
-            new_user.uid,
-            display_name = form.username.data,
-            phone_number = form.phone_number.data,
-            email_verified = False
-        )
+        else:
+            # Creating new user with email and password
+            new_user = auth.create_user(
+                email = form.email.data,
+                password = form.password.data
+            )
+            # Updating user's account with username and phone number
+            auth.update_user(
+                new_user.uid,
+                display_name = form.username.data,
+                phone_number = form.phone_number.data,
+                email_verified = False
+            )
+    else:
+        print("Form incorrect!")
 
     return render_template("temp_HTML/user_register.html", form = form)
 
+
+# DISCOVER
 @app.route('/discover')
 def discover():
 
@@ -71,9 +78,11 @@ def discover():
 
     return render_template("discover.html")
 
+
 @app.route('/home')
 def homecomp():
     return render_template("homecomp.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,16 +1,33 @@
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, validators
+from wtforms.validators import DataRequired, Email, ValidationError
+import phonenumbers
 
+# Phone number validator
+class E164PhoneNumberValidator:
+    def __call__(self, form, field):
+        try:
+            parsed_number = phonenumbers.parse(field.data)
+            if not phonenumbers.is_valid_number(parsed_number):
+                print("Invalid number")
+                raise ValidationError('Invalid E.164 phone number format')
+            
+            print("Valid number")
+
+        except phonenumbers.NumberParseException:
+            print("Invalid number")
+            raise ValidationError('Invalid E.164 phone number format')
+
+# User registration form
 class UserRegistrationForm(FlaskForm):
         first_name = StringField('First Name', validators=[DataRequired()])
         last_name = StringField('Last Name', validators=[DataRequired()])
         
         username = StringField('Username', validators=[DataRequired()])
-        phone_number = StringField('Phone Number', validators=[DataRequired()])
+        phone_number = StringField('Phone Number', validators=[DataRequired(), E164PhoneNumberValidator()])
         email = StringField('Email', validators=[DataRequired(), Email()])
         
-        password = PasswordField('Password', validators=[DataRequired()])
+        password = PasswordField('Password', validators=[DataRequired(), validators.length(min = 8)])
         confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
 
-        repatcha = RecaptchaField()
+        recaptcha = RecaptchaField()
