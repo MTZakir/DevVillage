2fa7f08311f1b8997796995414cf15caa4be9428
MTZakir <mtzakir13@outlook.com>
2024-02-29 16:58:53 +0400

    pull changes

--- app.py
+++ app.py
@@ -1,62 +1,37 @@
 from flask import Flask, render_template
+from auth_routes import auth_blueprint
+from discover_routes import discover_blueprint
+from dashboard_routes import dashboard_blueprint
 
 # Database
 import firebase_admin
-from firebase_admin import credentials, db, auth
+from firebase_admin import credentials
 
 # Initializing database
 cred = credentials.Certificate("codebase-secret.json")
 firebase_admin.initialize_app(cred, {"databaseURL": "https://codebase-93435-default-rtdb.europe-west1.firebasedatabase.app/"})
 
+# Initializing flask app
 app = Flask(__name__)
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(discover_blueprint)
+app.register_blueprint(dashboard_blueprint)
 
+# Applying flask configurations     --- To be kept secret ---
+app.config['SECRET_KEY'] = 'h#@hbJHB$@uygAHB!3137yugas_niGaJKH@#nlNAUBKJ~/AS,.69<>ASDfl..911,aSFOJ'
+app.config['RECAPTCHA_ENABLED'] = True
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcxaoEpAAAAAH92Ayj9QRJnLO8FRHDulED4OZOY'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcxaoEpAAAAAEOeRtpgYt_PDprYUmkVw_ryUl2p'
+
+# HOME
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-
-    # LOGIC FOR LOGIN
-
-    #user = auth.get_user_by_email("user@example.com")
-    #print(user.uid)
-
-    return render_template("login.html")
-
-@app.route('/register')
-def register():
-
-    # LOGIC FOR CREATING USER
-
-    #new_user = auth.create_user(
-    #    email = "user@example.com",
-    #    password = "ligma1024",
-    #)
-
-    #auth.update_user(
-    #    new_user.uid,
-    #    display_name = "Ligma Man",
-    #    phone_number = "+971549647315",
-    #    email_verified = False,
-    #    photo_url = "temp photo"
-    #)
-
-    return render_template("register.html")
-
-@app.route('/discover')
-def discover():
-
-    # LOGIC FOR DISCOVER
-
-    #ref = db.reference("/users")
-    #ref.update({"Chupapi": {"age": "6", "ball size": "2mm", "ball height": "0.1mm"}})
-
-    return render_template("discover.html")
-
 @app.route('/home')
 def homecomp():
     return render_template("homecomp.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
